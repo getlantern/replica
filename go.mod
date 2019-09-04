5d0d855478453dcdfde47920749f0c92c4df9a99
Matt Joiner <anacrolix@gmail.com>
2019-09-04 14:59:49 +1000

    Add stuff for flashlight

--- go.mod
+++ go.mod
@@ -7,7 +7,6 @@ require (
 	github.com/anacrolix/torrent v1.5.2
 	github.com/aws/aws-sdk-go v1.21.2
 	github.com/jawher/mow.cli v1.1.0
-	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7 // indirect
 	github.com/skratchdot/open-golang v0.0.0-20190402232053-79abb63cd66e
 	golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7
 )
--- replica.go
+++ replica.go
@@ -1,10 +1,10 @@
 package replica
 
 import (
-	"fmt"
 	"io"
 	"log"
 	"os"
+	"path"
 	"path/filepath"
 
 	_ "github.com/anacrolix/envpprof"
@@ -23,51 +23,58 @@ func newSession() *session.Session {
 	}))
 }
 
-func UploadFile(filename string) error {
+func Upload(f io.Reader, s3Key string) error {
 	sess := newSession()
-
-	// Create an uploader with the session and default options
 	uploader := s3manager.NewUploader(sess)
-
-	f, err := os.Open(filename)
-	if err != nil {
-		return fmt.Errorf("failed to open file %q: %v", filename, err)
-	}
-
-	s3Key := filepath.Base(filename)
-
-	// Upload the file to S3.
-	result, err := uploader.Upload(&s3manager.UploadInput{
+	_, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
 		Key:    aws.String(s3Key),
 		Body:   f,
 	})
 	if err != nil {
-		return xerrors.Errorf("failed to upload file, %w", err)
+		return xerrors.Errorf("uploading to s3: %w", err)
 	}
-	log.Printf("file uploaded to %q\n", result.Location)
-	fmt.Println(s3Key)
 	return nil
 }
 
-func GetTorrent(filename string) error {
+func UploadFile(filename string) (string, error) {
+	f, err := os.Open(filename)
+	if err != nil {
+		return "", xerrors.Errorf("opening file %q: %w", filename, err)
+	}
+	defer f.Close()
+	s3Key := filepath.Base(filename)
+	return s3Key, Upload(f, s3Key)
+}
+
+// Returns the object metainfo for the given key.
+func GetObjectTorrent(key string) (io.ReadCloser, error) {
 	sess := newSession()
 	svc := s3.New(sess)
 	out, err := svc.GetObjectTorrent(&s3.GetObjectTorrentInput{
 		Bucket: aws.String(bucket),
-		Key:    aws.String(filename),
+		Key:    aws.String(key),
 	})
 	if err != nil {
-		return err
+		return nil, err
 	}
-	defer out.Body.Close()
-	f, err := os.OpenFile(filepath.Base(filename)+".torrent", os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0640)
+	return out.Body, nil
+}
+
+// Downloads the metainfo for the Replica object to a .torrent file in the current working directory.
+func GetTorrent(key string) error {
+	t, err := GetObjectTorrent(key)
+	if err != nil {
+		return xerrors.Errorf("getting object torrent: %w", err)
+	}
+	defer t.Close()
+	f, err := os.OpenFile(path.Base(key)+".torrent", os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0640)
 	if err != nil {
 		return xerrors.Errorf("opening output file: %w", err)
 	}
 	log.Printf("created %q", f.Name())
 	defer f.Close()
-	if _, err := io.Copy(f, out.Body); err != nil {
+	if _, err := io.Copy(f, t); err != nil {
 		return xerrors.Errorf("copying torrent: %w", err)
 	}
 	if err := f.Close(); err != nil {
