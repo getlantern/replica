2bd3c7ac136f2ab8c7d2101ec71a433c762c8894
Matt Joiner <anacrolix@gmail.com>
2020-01-30 14:18:07 +1100

    Use magnetico filelists

--- search/src/bittorrent/magnetico.rs
+++ search/src/bittorrent/magnetico.rs
@@ -1,3 +1,8 @@
+use super::SearchResultItem;
+use anyhow::Context;
+use anyhow::Result;
+use log::*;
+use reqwest::Url;
 use serde::Deserialize;
 
 #[derive(Deserialize)]
@@ -12,4 +17,85 @@ pub struct Torrent {
     #[serde(rename = "nFiles")]
     pub n_files: u64,
     pub relevance: f64,
+}
+
+#[derive(Deserialize)]
+pub struct File {
+    pub size: i64,
+    pub path: String,
+}
+
+pub type Files = Vec<File>;
+
+pub struct Client {
+    root_url: Url,
+    http: reqwest::Client,
+}
+
+use std::borrow::Borrow;
+
+impl Client {
+    pub fn new() -> Self {
+        Self {
+            root_url: Url::parse("http://replica.anacrolix.link:8080/api/v0.1/").unwrap(),
+            http: reqwest::Client::new(),
+        }
+    }
+
+    async fn get<T, Q, K, V, P>(&self, path_segments: P, query_pairs: Q) -> Result<T, anyhow::Error>
+    where
+        T: serde::de::DeserializeOwned,
+        Q: IntoIterator,
+        Q::Item: Borrow<(K, V)>,
+        K: AsRef<str>,
+        V: AsRef<str>,
+        P: IntoIterator,
+        P::Item: AsRef<str>,
+    {
+        let mut url = self.root_url.clone();
+        url.query_pairs_mut().extend_pairs(query_pairs);
+        url.path_segments_mut().unwrap().extend(path_segments);
+        let response = self
+            .http
+            .get(url)
+            .basic_auth("derp", Some("secret"))
+            .send()
+            .await?;
+        let status = response.status();
+        response
+            .json::<T>()
+            .await
+            .with_context(|| format!("status: {}", status))
+    }
+
+    pub async fn list_files(&self, info_hash: &str) -> Result<Files> {
+        self.get(
+            &["torrents", info_hash, "filelist"],
+            std::iter::empty::<(&str, &str)>(),
+        )
+        .await
+    }
+
+    pub async fn search(&self, query: &str) -> Result<Vec<SearchResultItem>> {
+        let torrents: Vec<Torrent> = self.get(&["torrents"], &[("query", query)]).await?;
+        let mut ok = Vec::new();
+        for (t, fs) in futures_util::future::join_all(torrents.into_iter().map(|t| {
+            async move {
+                let files = self.list_files(&t.info_hash).await;
+                (t, files)
+            }
+        }))
+        .await
+        {
+            match fs {
+                Err(e) => error!("error getting files for {}: {}", t.info_hash, e),
+                Ok(files) => ok.extend(files.into_iter().map(|f| SearchResultItem {
+                    info_hash: t.info_hash.clone(),
+                    file_path: f.path,
+                    size: f.size,
+                })),
+            }
+        }
+        Ok(ok)
+    }
 }
--- search/src/server.rs
+++ search/src/server.rs
@@ -7,10 +7,24 @@ use crate::search::{self, OwnedMimeType};
 use crate::bittorrent;
 use log::*;
 
-#[derive(Serialize)]
+#[derive(Serialize, Default)]
 pub struct SearchResultItem {
-    pub key: String,
+    pub key: Option<String>,
     pub hits: usize,
+    pub info_hash: Option<String>,
+    pub file_path: Option<String>,
+    pub size: Option<bittorrent::FileSize>,
+}
+
+impl From<bittorrent::SearchResultItem> for SearchResultItem {
+    fn from(t: bittorrent::SearchResultItem) -> Self {
+        Self {
+            info_hash: Some(t.info_hash),
+            file_path: Some(t.file_path),
+            size: Some(t.size),
+            ..Default::default()
+        }
+    }
 }
 
 type SearchResult = Vec<SearchResultItem>;
@@ -22,18 +36,19 @@ pub async fn search_response(index: &IndexState, query: impl Into<search::Query>
         .unwrap()
         .get_matches(&index_query)
         .into_iter()
-        .map(|(key, hits)| SearchResultItem { key, hits })
+        .map(|(key, hits)| SearchResultItem {
+            key: Some(key),
+            hits,
+            ..Default::default()
+        })
         .collect();
     let query_value = index_query.terms.join(" ");
-    match bittorrent::search(query_value.as_str()).await {
-        Ok(more_results) => results.extend(more_results),
+    match bittorrent::Client::new().search(query_value.as_str()).await {
+        Ok(more_results) => results.extend(more_results.into_iter().map(Into::into)),
         Err(err) => error!("error searching bittorrent: {}", err),
     }
     results
 }
-
-#[deprecated(note = "search response result is now structured, use search_response instead")]
-pub use search_response as search_response_body;
 
 #[derive(Deserialize, Debug)]
 pub struct SearchQuery {
