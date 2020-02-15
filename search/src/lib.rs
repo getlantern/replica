f2e3c857d0efea12b9e17f4c611711afa7e8eb30
Max Bittman <max@npmjs.com>
2020-02-14 16:52:35 -0800

    Move search to separate repo

--- search/src/lib.rs
+++
@@ -1,9 +0,0 @@
-pub mod bittorrent;
-mod macros;
-mod replica;
-pub mod s3;
-pub mod search;
-pub mod server;
-mod singleflight;
-pub mod types;
-pub mod warp;
--- search/src/macros.rs
+++
@@ -1,9 +0,0 @@
-#[macro_export]
-macro_rules! handle {
-    ($value:expr, $err:ident, $on_err:expr) => {
-        match $value {
-            Ok(ok) => ok,
-            Err($err) => $on_err,
-        }
-    };
-}
