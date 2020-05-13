806c3b661981e6f0ac13013cedefe3e1fafaf08e
Matt Joiner <anacrolix@gmail.com>
2020-05-13 14:51:50 +1000

    Don't rely on S3's BitTorrent support
    Also import Replica link stuff from flashlight.

---
+++ link.go
@@ -0,0 +1,45 @@
+package replica
+
+import (
+	"fmt"
+	"net/url"
+
+	"github.com/anacrolix/torrent"
+	"github.com/anacrolix/torrent/metainfo"
+)
+
+func CreateLink(ih torrent.InfoHash, s3Prefix S3Prefix, fileName string) string {
+	return metainfo.Magnet{
+		InfoHash:    ih,
+		DisplayName: fileName,
+		Params: url.Values{
+			"as": {
+				fmt.Sprintf(
+					"https://getlantern-replica.s3-ap-southeast-1.amazonaws.com/%s/data/%s",
+					s3Prefix,
+					fileName,
+				),
+			},
+			"xs": {(&url.URL{Scheme: "replica", Opaque: s3Prefix.String()}).String()},
+			// This might technically be more correct, but I couldn't find any torrent client that
+			// supports it. Make sure to change any assumptions about "xs" before changing it.
+			//"xs": {fmt.Sprintf("https://getlantern-replica.s3-ap-southeast-1.amazonaws.com/%s/torrent", s3Prefix)},
+
+			// Since S3 key is provided, we know that it must be a single-file torrent.
+			"so": {"0"},
+		},
+	}.String()
+}
+
+// See CreateLink.
+func S3PrefixFromMagnet(m metainfo.Magnet) (S3Prefix, error) {
+	// url.Parse("") doesn't return an error! (which is currently what we want here).
+	u, err := url.Parse(m.Params.Get("xs"))
+	if err != nil {
+		return "", err
+	}
+	if u.Opaque != "" {
+		return S3Prefix(u.Opaque), nil
+	}
+	return S3Prefix(u.Path), nil
+}
