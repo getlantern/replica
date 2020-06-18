0f71f30b9dc661e3f38e06e3c0352b28f882e8b1
Matt Joiner <anacrolix@gmail.com>
2020-06-19 09:47:29 +1000

    Backward compatibility for upload metainfos

--- upload-metainfo.go
+++ upload-metainfo.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 
 	"github.com/anacrolix/torrent/metainfo"
+	"github.com/google/uuid"
 )
 
 // Wraps an upload metainfo.Metainfo
@@ -19,12 +20,23 @@ func (me *UploadMetainfo) FromTorrentMetainfo(mi *metainfo.MetaInfo) error {
 	if err != nil {
 		return fmt.Errorf("unmarshalling info: %w", err)
 	}
-	if len(info.Files) != 1 {
+	if len(info.UpvertedFiles()) != 1 {
 		return errors.New("expected single file")
 	}
 	*me = UploadMetainfo{
 		MetaInfo: mi,
 		Info:     info,
+	}
+	if mi.Comment == "Replica" {
+		u, err := uuid.Parse(info.Name)
+		if err != nil {
+			return fmt.Errorf("parsing uuid from info name: %w", err)
+		}
+		me.Upload = Upload{
+			UploadPrefix: UploadPrefix{u},
+			Endpoint:     DefaultEndpoint,
+		}
+		return nil
 	}
 	return me.Upload.FromExactSource(mi.Comment)
 }
