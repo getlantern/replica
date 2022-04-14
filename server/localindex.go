7ad5e29e242f4908d31c4cb4980ce2a91c87818e
Alec Soltzen <83119415+soltzen@users.noreply.github.com>
2022-04-14 19:03:36 +0200

    [localindex] Load last-downloaded backup index on startup
    Also, save downloaded backup indices in configDir

--- server/localindex.go
+++ server/localindex.go
@@ -11,7 +11,6 @@ import (
 	"path/filepath"
 	"strconv"
 	"strings"
-	"sync"
 	"time"
 
 	"crawshaw.io/sqlite"
@@ -24,22 +23,74 @@ import (
 	"github.com/getlantern/replica/service"
 )
 
+const LocalIndexFilenamePrefix = "replica-local-index"
+
+// Taken from https://gist.github.com/var23rav/23ae5d0d4d830aff886c3c970b8f6c6b
+// Because os.Rename() across directories will produce an "invalid cross-device
+// link" error
+func moveFile(sourcePath, destPath string) error {
+	inputFile, err := os.Open(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Couldn't open source file: %s", err)
+	}
+	outputFile, err := os.Create(destPath)
+	if err != nil {
+		inputFile.Close()
+		return fmt.Errorf("Couldn't open dest file: %s", err)
+	}
+	defer outputFile.Close()
+	_, err = io.Copy(outputFile, inputFile)
+	inputFile.Close()
+	if err != nil {
+		return fmt.Errorf("Writing to output file failed: %s", err)
+	}
+	// The copy was successful, so now delete the original file
+	err = os.Remove(sourcePath)
+	if err != nil {
+		return fmt.Errorf("Failed removing original file: %s", err)
+	}
+	return nil
+}
+
 type LocalIndexDhtDownloader struct {
 	// FullyDownloadedLocalIndexPath to the latest fully-downloaded local index
 	FullyDownloadedLocalIndexPath eventual.Value
 
+	configDir              string
 	res                    dhtup.Resource
 	lastSuccessfulInfohash metainfo.Hash
 }
 
+// RunLocalIndexDownloader does three things:
+// - Scans configDir for local index sqlite DBs that were already downloaded
+//   and assign it to FullyDownloadedLocalIndexPath
+// - Downloads the latest local index from the DHT
 func RunLocalIndexDownloader(
 	res dhtup.Resource,
 	checkForNewUpdatesEvery time.Duration,
+	configDir string,
 ) *LocalIndexDhtDownloader {
 	l := &LocalIndexDhtDownloader{
 		res:                           res,
 		FullyDownloadedLocalIndexPath: eventual.NewValue(),
+		configDir:                     configDir,
 	}
+
+	// Use the last fully-downloaded local index as the initial value
+	filepath.Walk(l.configDir, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+		if info.IsDir() {
+			return nil
+		}
+		if strings.HasPrefix(info.Name(), LocalIndexFilenamePrefix) {
+			log.Debugf("Found local index file %v", path)
+			l.FullyDownloadedLocalIndexPath.Set(path)
+		}
+		return nil
+	})
+
 	l.runDownloadRoutine(checkForNewUpdatesEvery)
 	return l
 }
@@ -52,10 +103,11 @@ func (me *LocalIndexDhtDownloader) runDownloadRoutine(checkForNewUpdatesEvery ti
 		err, hasNewIndex := me.download(ctx)
 		if err != nil {
 			log.Debugf(
-				"ignorable error while downloading local index. Will try again in %v: %v",
-				checkForNewUpdatesEvery, err)
-			// If an error occurs, retry immediately
+				"ignorable error while downloading local index. Will try again immediately: %v",
+				err)
+			// If an error occurs, wait a bit before trying again.
 			cancel()
+			time.Sleep(checkForNewUpdatesEvery / 2)
 			goto looper
 		}
 		// Else, re-run every "checkForNewUpdatesEvery"
@@ -77,16 +129,27 @@ func (me *LocalIndexDhtDownloader) runDownloadRoutine(checkForNewUpdatesEvery ti
 // in a temporary directory and set (or replace) the
 // LocalIndexDhtDownloader.FullyDownloadedLocalIndexPath value with the latest value.
 func (me *LocalIndexDhtDownloader) download(ctx context.Context) (err error, hasNewIndex bool) {
+	log.Debugf("Attempting to fetch Bep46 of Replica local index")
 	// Check if there's a new infohash to download (or download the last
 	// infohash if this is our first run)
 	bep46PayloadInfohash, err := me.res.FetchBep46Payload(ctx)
 	if err != nil {
 		return log.Errorf("while fetching bep46 payload: %v", err), false
 	}
+	// TODO <14-04-2022, soltzen> There's a weird edge case here:
+	// - if there was a downloaded local index file,
+	// - and it matches the infohash of the bep46 payload,
+	// - we'll still retrigger the download.
+	//
+	// This happens since we don't save the last infohash we downloaded. We
+	// really should. The optimization here is that this'll save the user a
+	// single download upon launch
 	if bytes.Equal(me.lastSuccessfulInfohash[:], bep46PayloadInfohash[:]) {
 		// No new infohash
 		return nil, false
 	}
+	log.Debugf("Fetched new infohash for Replica local index %s",
+		bep46PayloadInfohash.HexString())
 
 	// Fetch the torrent's io.ReadCloser and attempt to download it with
 	// io.Copy
@@ -94,32 +157,56 @@ func (me *LocalIndexDhtDownloader) download(ctx context.Context) (err error, has
 	if err != nil {
 		return err, false
 	}
+	log.Debugf(
+		"Fetched torrent io.reader for Replica local index %s. Attempting to download...",
+		bep46PayloadInfohash.HexString())
 	ctxReader := missinggo.ContextedReader{R: r, Ctx: ctx}
 	fd, err := os.CreateTemp("", "replica-local-index")
 	if err != nil {
-		return log.Errorf("Making local index file: %v", err), false
+		return log.Errorf("making local index file: %v", err), false
 	}
 	defer fd.Close()
-	// TODO <23-03-22, soltzen> Should we have a context-aware reader here?
 	_, err = io.Copy(fd, ctxReader)
 	if err != nil {
-		return log.Errorf("Copying local index torrent reader: %v", err), false
+		os.Remove(fd.Name())
+		return log.Errorf("downloading local index torrent reader: %v", err), false
 	}
 
 	// Before replacing with the new value, see if there's already a value in
-	// FullyDownloadedLocalIndexPath. If so, reset the eventual.Value to avoid race conditions and
-	// unlink the file
+	// FullyDownloadedLocalIndexPath. If so, reset the eventual.Value to avoid
+	// race conditions and unlink the file
 	expiredCtx, cancel := context.WithCancel(context.Background())
 	cancel()
 	v, _ := me.FullyDownloadedLocalIndexPath.Get(expiredCtx)
 	// XXX <28-03-2022, soltzen> We don't call what's the error: just care
 	// whether there's a value set
 	if v != nil {
+		log.Debugf("Found old Replica local index file. Unlinking...")
 		me.FullyDownloadedLocalIndexPath.Reset()
 		os.Remove(v.(string))
 	}
 
-	me.FullyDownloadedLocalIndexPath.Set(fd.Name())
+	// Move the file to configDir. This is necessary so that the initial
+	// startup of Lantern picks it up instead of waiting for a redownload.
+	// Also, name it with LocalIndexFilenamePrefix so that it's picked up by
+	// filepath.Walk() and assigned to me.FullyDownloadedLocalIndexPath as the
+	// initial value, if it exists
+	n := time.Now()
+	newPath := filepath.Join(me.configDir,
+		fmt.Sprintf("/%s-%s.sqlite",
+			LocalIndexFilenamePrefix,
+			n.Format("20060102-150405")))
+	err = moveFile(fd.Name(), newPath)
+	if err != nil {
+		os.Remove(fd.Name())
+		return log.Errorf("moving downloaded local index file from %s to %s: %v",
+			fd.Name(), newPath, err), false
+	}
+
+	log.Debugf(
+		"Successfully downloaded Replica local index %s. Setting path to %v",
+		bep46PayloadInfohash.HexString(), newPath)
+	me.FullyDownloadedLocalIndexPath.Set(newPath)
 	return nil, true
 }
 
