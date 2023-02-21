6e0de9ff7c90f0a74c30b174f5e5bdb654957fc0
Ox Cart <ox.to.a.cart@gmail.com>
2023-02-21 06:41:38 -0600

    Decouple from flashlight

--- config/config.go
+++ config/config.go
@@ -3,9 +3,8 @@ package replicaConfig
 import (
 	"net/url"
 
-	"github.com/getlantern/flashlight"
-	"github.com/getlantern/flashlight/config"
-	"github.com/getlantern/flashlight/geolookup"
+	"github.com/mitchellh/mapstructure"
+
 	"github.com/getlantern/golog"
 
 	replicaServer "github.com/getlantern/replica/server"
@@ -14,6 +13,11 @@ import (
 
 var log = golog.LoggerFor("replica.config")
 
+// mimics the same interface from Flashlight
+type FeatureOptions interface {
+	FromMap(map[string]interface{}) error
+}
+
 type ReplicaOptionsGetter func() replicaServer.ReplicaOptions
 
 // This returns a function that extracts the ReplicaOptions for the country determined by
@@ -21,24 +25,25 @@ type ReplicaOptionsGetter func() replicaServer.ReplicaOptions
 // refreshing is done consistently as per android-lantern and lantern-desktop. The function does not
 // fail, it will always return a reasonable fallback option.
 func NewReplicaOptionsGetter(
-	f *flashlight.Flashlight,
+	populateReplicaOptions func(FeatureOptions) error,
 	getCountry func() (string, error),
+	refreshGeolocation func(),
 ) ReplicaOptionsGetter {
 	return func() replicaServer.ReplicaOptions {
-		var root config.ReplicaOptionsRoot
-		if err := f.FeatureOptions(config.FeatureReplica, &root); err != nil {
+		var root ReplicaOptionsRoot
+		if err := populateReplicaOptions(&root); err != nil {
 			log.Errorf(
 				"Could not fetch replica feature options: %v",
 				err,
 			)
-			geolookup.Refresh()
+			refreshGeolocation()
 			return replicaServer.FallbackReplicaOptions{}
 		}
 		countryCode, err := getCountry()
 		if err != nil {
 			log.Errorf("while selecting replica options: error getting country: %v", err)
-			log.Debugf("refreshing geolookup")
-			geolookup.Refresh()
+			log.Debugf("refreshing geolocation")
+			refreshGeolocation()
 		}
 		opts, ok := root.ByCountry[countryCode]
 		if ok {
@@ -72,4 +77,69 @@ func GetReplicaServiceEndpointUrl(opts replicaServer.ReplicaOptions) *url.URL {
 	}
 	log.Debugf("using replica rust endpoint %q", url.String())
 	return url
+}
+
+type ReplicaOptionsRoot struct {
+	// This is the default.
+	ReplicaOptions `mapstructure:",squash"`
+	// Options tailored to country. This could be used to pattern match any arbitrary string really.
+	// mapstructure should ignore the field name.
+	ByCountry map[string]ReplicaOptions `mapstructure:",remain"`
+	// Deprecated. An unmatched country uses the embedded ReplicaOptions.ReplicaRustEndpoint.
+	// Removing this will break unmarshalling config.
+	ReplicaRustDefaultEndpoint string
+	// Deprecated. Use ByCountry.ReplicaRustEndpoint.
+	ReplicaRustEndpoints map[string]string
+}
+
+// Implements the interface FeatureOptions from flashlight
+func (ro *ReplicaOptionsRoot) FromMap(m map[string]interface{}) error {
+	return mapstructure.Decode(m, ro)
+}
+
+type ReplicaOptions struct {
+	// Use infohash and old-style prefixing simultaneously for now. Later, the old-style can be removed.
+	WebseedBaseUrls []string
+	Trackers        []string
+	StaticPeerAddrs []string
+	// Merged with the webseed URLs when the metadata and data buckets are merged.
+	MetadataBaseUrls []string
+	// The replica-rust endpoint to use. There's only one because object uploads and ownership are
+	// fixed to a specific bucket, and replica-rust endpoints are 1:1 with a bucket.
+	ReplicaRustEndpoint string
+	// A set of info hashes (20 bytes, hex-encoded) to which proxies should announce themselves.
+	ProxyAnnounceTargets []string
+	// A set of info hashes where p2p-proxy peers can be found.
+	ProxyPeerInfoHashes []string
+	CustomCA            string
+}
+
+func (ro *ReplicaOptions) GetWebseedBaseUrls() []string {
+	return ro.WebseedBaseUrls
+}
+
+func (ro *ReplicaOptions) GetTrackers() []string {
+	return ro.Trackers
+}
+
+func (ro *ReplicaOptions) GetStaticPeerAddrs() []string {
+	return ro.StaticPeerAddrs
+}
+
+func (ro *ReplicaOptions) GetMetadataBaseUrls() []string {
+	return ro.MetadataBaseUrls
+}
+
+func (ro *ReplicaOptions) GetReplicaRustEndpoint() string {
+	return ro.ReplicaRustEndpoint
+}
+
+func (ro *ReplicaOptions) GetCustomCA() string {
+	return ro.CustomCA
+}
+
+// XXX <11-07-2022, soltzen> DEPREACTED in favor of
+// github.com/getlantern/libp2p
+func (ro *ReplicaOptions) GetProxyAnnounceTargets() []string {
+	return nil
 }
